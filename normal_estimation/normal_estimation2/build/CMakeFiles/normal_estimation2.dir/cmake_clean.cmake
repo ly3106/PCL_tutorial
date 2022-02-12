c0bbb034e10bffc14e44a8c9cdca83c3339fd2b5
ly3106 <869915532@qq.com>
2022-02-12 17:31:54 +0800

    first commit

---
+++ normal_estimation/normal_estimation2/build/CMakeFiles/normal_estimation2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal_estimation2.dir/normal_estimation2.cpp.o"
+  "normal_estimation2.pdb"
+  "normal_estimation2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal_estimation2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ normal_estimation/normal_estimation3/build/CMakeFiles/normal_estimation3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal_estimation3.dir/normal_estimation3.cpp.o"
+  "normal_estimation3.pdb"
+  "normal_estimation3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal_estimation3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
